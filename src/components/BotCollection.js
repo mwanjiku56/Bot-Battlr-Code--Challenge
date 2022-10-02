b72f3df95637876e300937ba9c622d9c2c2ffade
Mercy Maruga <mercy.maruga@student.moringaschool.com>
2022-10-02 19:43:20 +0300

    Add a list of all bots in the collection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState,useEffect } from "react";
+import BotCard from "./BotCard";
 
 function BotCollection() {
-  // Your code here
+
+  const [allBots,setAllBots]=useState([])
+
+
+  useEffect(() => {
+    fetch( "http://localhost:8002/bots")
+      .then((r) => r.json())
+      .then((allBots) => setAllBots(allBots));
+  }, []);
+
+
+
   return (
     <div className="ui four column grid">
       <div className="row">
         {/*...and here..*/}
+        {allBots.map(bot=>(
+         <BotCard key={bot.id} bot={bot} />))}
         Collection of all bots
       </div>
     </div>
   );
-}
 
+}
 export default BotCollection;
